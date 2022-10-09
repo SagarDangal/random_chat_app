51a1344ec77c56c4f9599cb14feec42c8aec3716
Sag <ragaslagnad28@gmail.com>
2022-10-09 20:44:22 +0545

    initial commit

--- app/db/deps.py
+++ app/db/deps.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import urllib
 
-MONGO_SRV = 'mongodb+srv://Sagar:'+urllib.parse.quote('Appleball@12345')+'@chat.o9rso.mongodb.net/Chat?retryWrites=true&w=majority'
+MONGO_SRV = 'mongodb+srv://Sagar:'+urllib
 
 # DB
 mongo_client = MongoClient(MONGO_SRV)
